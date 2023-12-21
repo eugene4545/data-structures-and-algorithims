2083c1dad6a7896e97b69d1335824e24d704d8d3
eugene4545 <eugeneonuoha72@gmail.com>
2023-12-21 21:28:30 +0100

    twosum

--- leetcode/Two-Sum/twoSum.js
+++ leetcode/Two-Sum/twoSum.js
@@ -37,25 +37,5 @@ Time complexity: O(n) - worse case, there are no pairs and we iterate over every
 Space complexity: O(n) - we are using map to store the pairs
 
 Code
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-/*
-var twoSum = function(nums, target) {
-    let mp = new Map()
-    
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        
-        if (mp.has(diff)) {
-            return [i, mp.get(diff)]
-        }
-        
-        mp.set(nums[i], i)
-    }
-};
-*/
 
 
