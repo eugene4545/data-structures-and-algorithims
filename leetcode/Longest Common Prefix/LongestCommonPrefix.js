e25fcc74917060c2ba5f67a2b07a656404e86ed0
eugene4545 <eugeneonuoha72@gmail.com>
2024-03-27 10:21:53 +0100

    alternative solution

--- leetcode/Longest Common Prefix/LongestCommonPrefix.js
+++ leetcode/Longest Common Prefix/LongestCommonPrefix.js
@@ -19,3 +19,39 @@ var longestCommonPrefix = function(strings) {
     // If all strings have the same prefix, return the first string
     return strings[0];
 };
+
+
+
+
+
+// an alternative solution with slightly better time complexity
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let word=""; //the variable that takes every character of the first string one by one
+    let words="";// the variable that stores the previous state of 'word' variable
+    let i; // variable for iterating through the remaining strings
+    let count=0; //To let the code know which character of the string to look at
+    while(count!=strs[0].length) // A loop to gather every character of the first string at 0th index
+    {
+        words=word; // stores the previous state
+        word+=strs[0][count]; //adds the next character to 'word' variable's previous state
+        for(i=1;i<strs.length;i++) //A loop to iterate through the remaining strings
+        {
+            if(strs[i][count]!==word[count]) //condition for 'word' & 'count' number of character of remaining strings 
+            {
+                // if true then revert to previous state and break
+                word=words; //reverting to previous state
+                break;
+            }
+        }
+        if(word==words) //if state of 'word' remains unchanged then break
+        {
+            break;
+        }
+        count+=1; //increment to count to get the next character
+    }
+    return(word); //answer
+};
